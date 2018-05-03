db31aacdc99d955010e081b59c1464d0239a69a5
trilobit-gmbh <trilobit-gmbh@users.noreply.github.com>
2018-05-03 15:40:20 +0200

    Update HookOutputFrontendTemplate.php

--- src/Resources/contao/classes/HookOutputFrontendTemplate.php
+++ src/Resources/contao/classes/HookOutputFrontendTemplate.php
@@ -14,6 +14,7 @@
 namespace Trilobit\IfthenelseBundle;
 
 use Contao\Controller;
+use Contao\Input;
 use Contao\Template;
 
 /**
@@ -22,6 +23,48 @@ use Contao\Template;
  */
 class HookOutputFrontendTemplate extends Template
 {
+
+    /**
+     * @param $varValue
+     * @param bool $blnClean
+     * @return mixed|string
+     */
+    protected function searchReplace($varValue, $blnClean=true)
+    {
+        $varValue = str_replace(
+            array(
+                '__BRCL__', '__BRCR__',
+                '[gt]', '&gt;', '&#62;',
+                '[lt]', '&lt;', '&#60;',
+                '[&]', '&amp;', '&#38;',
+                '&#61;',
+            ),
+            array(
+                '{{', '}}',
+                '>', '>', '>',
+                '<', '<', '<',
+                '&', '&', '&',
+                '=',
+            ),
+            $varValue
+        );
+
+        $varValue = trim($varValue);
+
+        $varValue = Controller::replaceInsertTags($varValue, false);
+
+        if ($blnClean)
+        {
+            $varValue = Input::cleanKey($varValue);
+            $varValue = Input::encodeSpecialChars($varValue);
+        }
+
+        $varValue = str_replace("'", "\'", $varValue);
+
+        return $varValue;
+    }
+
+
     /**
      * @param $strBuffer
      * @param $objTemplate
@@ -39,54 +82,85 @@ class HookOutputFrontendTemplate extends Template
             $blnEval = false;
             $strReturn = '';
 
+            $arrOperations = array(
+                '<',
+                '<=',
+                '>',
+                '>=',
+                '==',
+                '===',
+                '!=',
+                '!==',
+            );
+
             // Replace tags
             foreach ($arrTags as $strTag)
             {
                 $strCondition = '';
 
-                if (strncmp($strTag, '{if', 3) === 0)
+                if (strncmp($strTag, '{if ', 4) === 0)
                 {
-                    $strCondition = preg_replace('/\{if (.*)\}/i', '<?php if ($1): ?>', $strTag);
+                    preg_match_all('/\{if \'(.*)\'(.*?)\'(.*)\'\}/i', $strTag, $arrCondition);
+
+                    if (count($arrCondition) !== 4)
+                    {
+                        throw new \Exception("Error condition-string [" . $strTag . "] in Trilobit\IfthenelseBundle\HookOutputFrontendTemplate::replaceConditionalTags()");
+                    }
+
+                    $token    = self::searchReplace($arrCondition[1][0]);
+                    $operator = self::searchReplace($arrCondition[2][0], false);
+                    $value    = self::searchReplace($arrCondition[3][0]);
+
+                    var_dump(array(
+                        $token,
+                        $operator,
+                        $value
+                    ));
+
+                    if (!in_array($operator, $arrOperations))
+                    {
+                        throw new \Exception("Error condition (if-operator [" . $operator . "]) in Trilobit\IfthenelseBundle\HookOutputFrontendTemplate::replaceConditionalTags()");
+                    }
+
+                    $strCondition = '<?php if (\'' . $token . '\' ' . $operator . ' \'' . $value . '\'): ?>';
                     $blnEval = true;
                 }
-                elseif (strncmp($strTag, '{elseif', 7) === 0)
+                elseif (strncmp($strTag, '{elseif ', 8) === 0)
                 {
-                    $strCondition = preg_replace('/\{elseif (.*)\}/i', '<?php elseif ($1): ?>', $strTag);
+                    preg_match_all('/\{elseif \'(.*)\'(.*?)\'(.*)\'\}/i', $strTag, $arrCondition);
+
+                    if (count($arrCondition) !== 4)
+                    {
+                        throw new \Exception("Error condition-string [" . $strTag . "] in Trilobit\IfthenelseBundle\HookOutputFrontendTemplate::replaceConditionalTags()");
+                    }
+
+                    $token    = self::searchReplace($arrCondition[1][0]);
+                    $operator = self::searchReplace($arrCondition[2][0], false);
+                    $value    = self::searchReplace($arrCondition[3][0]);
+
+                    if (!in_array($operator, $arrOperations))
+                    {
+                        throw new \Exception("Error condition (elseif-operator [" . $operator . "]) in Trilobit\IfthenelseBundle\HookOutputFrontendTemplate::replaceConditionalTags()");
+                    }
+
+                    $strCondition = '<?php elseif (\'' . $token . '\' ' . $operator . ' \'' . $value . '\'): ?>';
                     $blnEval = true;
                 }
-                elseif (strncmp($strTag, '{else', 5) === 0)
+                elseif (strncmp($strTag, '{else}', 6) === 0)
                 {
                     $strCondition = '<?php else: ?>';
                     $blnEval = true;
                 }
-                elseif (strncmp($strTag, '{endif', 6) === 0)
+                elseif (strncmp($strTag, '{endif}', 7) === 0)
                 {
                     $strCondition = '<?php endif; ?>';
                     $blnEval = true;
                 }
 
+
                 if ($strCondition !== '')
                 {
-                    $strReturn .= Controller::replaceInsertTags(
-                        str_replace(
-                            array(
-                                '__BRCL__', '__BRCR__',
-                                '[gt]', '&gt;', '&#62;',
-                                '[lt]', '&lt;', '&#60;',
-                                '[&]', '&amp;', '&#38;',
-                                '&#61;'
-                            ),
-                            array(
-                                '{{', '}}',
-                                '>', '>', '>',
-                                '<', '<', '<',
-                                '&', '&', '&',
-                                '='
-                            ),
-                            $strCondition
-                        ),
-                        false
-                    );
+                    $strReturn .= $strCondition;
                 }
                 else
                 {
@@ -98,10 +172,20 @@ class HookOutputFrontendTemplate extends Template
 
             if ($blnEval)
             {
-                //var_dump($strReturn);
-                //die();
-
                 ob_start();
+
+                /*
+                 * ACHTUNG!
+                 *
+                 * ohne das '?>' kommt es zu folgendem Fehler:
+                 *
+                 * - app.CRITICAL: An exception occurred.
+                 *   {
+                 *       "exception":
+                 *       "[object] (Symfony\\Component\\Debug\\Exception\\FatalThrowableError(code: 0): Parse error: syntax error, unexpected end of file at /export/Daten/contao/other/gasq/contao4/www/vendor/trilobit-gmbh/contao-ifthenelse-bundle/src/Resources/contao/classes/HookOutputFrontendTemplate.php:184)"
+                 *   } []
+                 *
+                 */
                 $blnEval = eval('?>' . $strBuffer);
                 $strBuffer = ob_get_contents();
                 ob_end_clean();
@@ -111,7 +195,6 @@ class HookOutputFrontendTemplate extends Template
                     throw new \Exception("Error eval() in Trilobit\IfthenelseBundle\HookOutputFrontendTemplate::replaceConditionalTags()");
                 }
             }
-
         }
 
         $strBuffer = str_replace(array('__BRCL__', '__BRCR__'), array('{{', '}}'), $strBuffer);
